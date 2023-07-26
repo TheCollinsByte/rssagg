8af556589bfe52fedcd364db0d0bfe102c5dc6ee
colspan <collin@collindigm.com>
2023-07-26 10:59:10 +0300

    Server configuration route and cors

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/paradigm-lab/rssagg
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/joho/godotenv"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
 )
-
 
 func main() {
 
-    godotenv.Load(".env")
+	godotenv.Load(".env")
 
-    portString := os.Getenv("PORT")
-    if portString == "" {
-        log.Fatal("PORT is not found in the environment")
-    }
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
 
-    fmt.Println("Port:", portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server Starting on port %v", portString)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
