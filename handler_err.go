43738d3e82e0a29b59c8c2429acaafeea0599304
colspan <collin@collindigm.com>
2023-07-26 11:27:04 +0300

    Error Handler

--- handler_err.go
+++ handler_err.go
@@ -2,6 +2,6 @@ package main
 
 import "net/http"
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func handlerErr(w http.ResponseWriter, r *http.Request) {
 	respondWithError(w, 400, "Something Went Wrong")
 }
