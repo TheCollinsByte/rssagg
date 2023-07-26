e97e21ea8e48fbc16ebe2b5f7ac8e06cc646b3b6
colspan <collin@collindigm.com>
2023-07-26 10:19:11 +0300

    package env imported

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
     "fmt"
     "log"
     "os"
+
+    "github.com/joho/godotenv"
 )
 
 
 func main() {
+
+    godotenv.Load(".env")
+
     portString := os.Getenv("PORT")
     if portString == "" {
         log.Fatal("PORT is not found in the environment")
