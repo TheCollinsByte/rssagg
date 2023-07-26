483ba8119e64c6f2638d2492a13fefa80cd02713
colspan <collin@collindigm.com>
2023-07-26 11:09:45 +0300

    bug fix

--- json.go
+++ json.go
@@ -16,6 +16,6 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	}
 
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(dat)
 }
