4f9893f161cc076b1e153eda74e32138526eba24
colspan <collin@collindigm.com>
2023-08-01 13:41:43 +0300

    CreateUser Handler for the request and response

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,41 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/paradigm-lab/rssagg/internal/database"
+)
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, 200, struct{}{})
+
+	type parameters struct {
+		Name string `json: "name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON:", err))
+		return
+	}
+
+	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't Create user:", err))
+		return
+	}
+
+	respondWithJSON(w, 200, user)
 }
